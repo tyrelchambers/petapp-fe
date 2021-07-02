662f8d0ecf8a616d65bd69d4f518cf13204894dc
Tyrel Chambers <tychambers3@gmail.com>
2021-07-02 13:20:03 -0400

    init

---
+++ src/layouts/Wrapper/Wrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Wrapper = (props) => {
+  return <div className="max-w-screen-xl">{props.children}</div>;
+};
+
+export default Wrapper;
---
+++ src/pages/Index/Index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Wrapper from "../../layouts/Wrapper/Wrapper";
+
+const Index = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default Index;
